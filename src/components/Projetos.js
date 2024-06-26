b3fe753c4f0064130aea7faf68691f49ded3b43d
GranFireball <leonardohk10@hotmail.com>
2024-06-25 21:31:20 -0300

    update projects

--- src/components/Projetos.js
+++ src/components/Projetos.js
@@ -3,11 +3,12 @@ import CardProjeto from "./CardProjeto"
 import capaProjetoReceitas from "../imgs/projetoReceitas.PNG";
 import capaProjetoMapaAstral from "../imgs/projetoMapaAstral.PNG";
 import capaProjetoNumberGame from "../imgs/projetoNumberGame.PNG";
+import capaProjetoLobbyGamer from "../imgs/projetoLobbyGamer.png";
 
 export default function Projetos(){
   const projetos = [
     {nome: "Projeto de Receitas", capa: capaProjetoReceitas, link: "https://projeto-receitas-unidas.vercel.app/", detalhes: "(Encontre novas receitas)"},
-    {nome: "Projeto de Mapa Astral", capa: capaProjetoMapaAstral, link: "https://projeto-mapa-astral.vercel.app/", detalhes: "(Explore o mapa astral)"},
+    {nome: "Projeto de Chat", capa: capaProjetoLobbyGamer, link: "https://lobby-gamer.vercel.app/", detalhes: "(Chat em tempo real)"},
     {nome: "Projeto de Jogo", capa: capaProjetoNumberGame, link: "https://number-game-project.vercel.app/", detalhes: "(Adivinhe o número)"},
   ]
   return(
